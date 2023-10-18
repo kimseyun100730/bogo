6fdbedfed631a00e23e4543075790730412df9e8
kimseyun100730 <77229097+kimseyun100730@users.noreply.github.com>
2023-10-19 07:44:18 +0900

    Add files via upload

---
+++ test.php
@@ -0,0 +1,10 @@
+﻿<!DOCTYPE html>
+<html>
+<body>
+<form action="upload.php" method="post" enctype="multipart/form-data" target="test">
+    <input type="file" name="fileToUpload[]" multiple='multiple'>
+    <input type="submit">
+</form>
+<iframe name="test"></iframe><br>
+</body>
+</html>
